722a856d7516ae049673d714f07fb49475cef498
Mercedes1501 <mercedes1501@hotmail.com>
2020-03-22 18:38:20 +0100

    Ajout de la fonction nettoyage body

--- code_final.py
+++ code_final.py
@@ -12,8 +12,6 @@ from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 from nltk import FreqDist
 
-model_sgd = pickle.load(open('model_SGD.pkl', 'rb'))
-mlb = pickle.load(open('mlb_fit.pkl', 'rb'))
 model_lda = pickle.load(open('lda.pkl', 'rb'))
 count = pickle.load(open('countvecto.pkl', 'rb'))
 
@@ -45,7 +43,44 @@ def review_to_words(raw_review):
     # and return the result.
     return(" ".join(lemmatized_word))
 
-
+def review_to_body(raw_review):
+    # Function to convert a raw review to a string of words
+    # The input is a single string, and
+    # the output is a single string
+    #
+    # 1. Remove HTML
+    review_text = BeautifulSoup(raw_review).get_text()
+    #
+    # 2. Remove capital-letters
+    lower_text = review_text.lower()
+    #
+    # 3. Remove non-letters
+    letters_only = re.sub("[^a-zA-Z]", " ", lower_text)
+    #
+    # 4. Keep only names> 2
+    new_string = ' '.join([w for w in letters_only.split() if len(w) > 2])
+    #
+    # 5. Remove non-letters
+    tokenizer = nltk.RegexpTokenizer(r'\w+')
+    words = tokenizer.tokenize(new_string)
+    #
+    # 6. Remove stop words
+    stopword = stopwords.words("english")
+    removing_stopwords = [word for word in words if word not in stopword]
+    #
+    # 7. Lemmanization
+    wordnet_lemmatizer = WordNetLemmatizer()
+    lemmatized_word = [wordnet_lemmatizer.lemmatize(word)
+                       for word in removing_stopwords]
+    #
+    # 8. Keep only names
+    tags = nltk.pos_tag(lemmatized_word)
+    nouns = [word for word, pos in tags if
+             (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS')]
+    #
+    # 9. Join the words back into one string separated by space,
+    # and return the result.
+    return(" ".join(nouns))
 
 def predict_tags_sup(question):
     
